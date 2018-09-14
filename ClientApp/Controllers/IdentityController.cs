3fe3ce8a09296f1f6f64728a25ea0490f1de6e13
Paul Muir <pmuir@dcthomson.co.uk>
2018-09-14 13:21:28 +0100

    Add project files.

---
+++ ClientApp/Controllers/IdentityController.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientApp.Controllers
+{
+    public class IdentityController : Controller
+    {
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            var discovery = await DiscoveryClient.GetAsync("http://localhost:5000");
+
+            var tokenClient = new TokenClient(discovery.TokenEndpoint, "clientApp", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("apiApp");
+
+            ViewData["tokenResult"] = tokenResponse.IsError ? tokenResponse.Error : tokenResponse.Json.ToString();
+
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var apiResponse = await client.GetAsync("http://localhost:5001/api/identity");
+            ViewData["apiResult"] = apiResponse.IsSuccessStatusCode ? await apiResponse.Content.ReadAsStringAsync() : apiResponse.StatusCode.ToString();
+
+            return View();
+        }
+
+        public async Task Logout()
+        {
+            await HttpContext.Authentication.SignOutAsync("Cookies");
+            await HttpContext.Authentication.SignOutAsync("oidc");
+        }
+    }
+}
